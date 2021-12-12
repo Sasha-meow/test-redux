f997fe3454b3d370f9b2d0f58d48b58da09a31e5
Sasha-meow <aleksandragromkova@gmail.com>
2021-12-12 20:30:02 +0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import {useState} from "react";
+import axios from "axios";
+
+function App() {
+  const [items, setItems] = useState([])
+  const getData = () => {
+    axios({
+      method: 'get',
+      headers: { 'accept': '*/*'},
+      url: 'http://c286-185-253-182-161.ngrok.io/project/view/registry/',
+    }).then(function (response) {
+      setItems(response.data)
+      console.log(response.data);
+    });
+  }
+  return (
+    <div className="App">
+      <button onClick={() => getData()}>Клик</button>
+      {items.length ? items.map((item) =>
+        (<div key={item.id}>{item.companyName}</div>)
+      ):(<div>Упс</div>)}
+    </div>
+  );
+}
+
+export default App;
